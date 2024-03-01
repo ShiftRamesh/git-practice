3de4a2232dc3be6d6853fc7d7c1953de361c3595
ramachandrarao971 <ramachandrarao971@gmail.com>
2024-03-01 14:20:24 +0530

    Commitng My Addition.java & Bgm.jpg

---
+++ Additon.java
@@ -0,0 +1,24 @@
+class Additon
+{
+
+byte a=112;
+int b=218;
+
+void add()
+{
+int c=a+b;
+System.out.println(a+" =is value of A");
+System.out.println(b+" =is value of B");
+System.out.println("hi"+a+"Hello");
+
+System.out.println("Addition of A & B = "+c);
+
+}
+
+public static void main(String[]args)
+{
+Additon ad=new Additon();
+ad.add();
+}
+
+}
